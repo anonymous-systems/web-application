3c3b11819739ecd6f885b3010aae4eb9d1455ec3
Aaron Jones II <jonesaaron1996@gmail.com>
2024-07-26 05:47:21 -0400

    fix(eslint): use correct eslint file

--- projects/frontend/eslint.config.js
+++ projects/frontend/eslint.config.js
@@ -1,32 +1,7 @@
 // @ts-check
-const tseslint = require("typescript-eslint");
-const rootConfig = require("../../eslint.config.js");
+const tsEslint = require("typescript-eslint");
+const workspaceEslint = require("../../eslint.config");
 
-module.exports = tseslint.config(
-  ...rootConfig,
-  {
-    files: ["**/*.ts"],
-    rules: {
-      "@angular-eslint/directive-selector": [
-        "error",
-        {
-          type: "attribute",
-          prefix: "app",
-          style: "camelCase",
-        },
-      ],
-      "@angular-eslint/component-selector": [
-        "error",
-        {
-          type: "element",
-          prefix: "app",
-          style: "kebab-case",
-        },
-      ],
-    },
-  },
-  {
-    files: ["**/*.html"],
-    rules: {},
-  }
+module.exports = tsEslint.config(
+    ...workspaceEslint,
 );
