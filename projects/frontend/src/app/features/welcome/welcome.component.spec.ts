b002ef8692b11438afab4eb16867cdff801dff89
Aaron Jones II <jonesaaron1996@gmail.com>
2024-07-26 23:42:37 -0400

    fix(frontend): welcome test

--- projects/frontend/src/app/features/welcome/welcome.component.spec.ts
+++ projects/frontend/src/app/features/welcome/welcome.component.spec.ts
@@ -1,17 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { WelcomeComponent } from './welcome.component';
+import { ActivatedRoute } from "@angular/router";
 
 describe('WelcomeComponent', () => {
   let component: WelcomeComponent;
   let fixture: ComponentFixture<WelcomeComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [WelcomeComponent]
-    });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [WelcomeComponent],
+      providers: [
+        { provide: ActivatedRoute, useValue: {} },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(WelcomeComponent);
+
     component = fixture.componentInstance;
+
     fixture.detectChanges();
   });
 
