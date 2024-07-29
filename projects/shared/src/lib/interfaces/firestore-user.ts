9eb2777eddfeba5fd1061721894e2da4028530ad
Aaron Jones II <jonesaaron1996@gmail.com>
2024-07-28 23:09:28 -0400

    fix(shared): firestore user interface to extend user info

--- projects/shared/src/lib/interfaces/firestore-user.ts
+++ projects/shared/src/lib/interfaces/firestore-user.ts
@@ -1,10 +1,11 @@
+import { UserInfo } from "@angular/fire/auth";
 import {
   FirestoreDocument,
   ReadFirestoreDocument,
   WriteFirestoreDocument
 } from "./firestore-document";
 
-export interface FirestoreUser extends FirestoreDocument {
+export interface FirestoreUser extends UserInfo, FirestoreDocument {
   username: string | null,
   firstName: string | null,
   lastName: string | null,
