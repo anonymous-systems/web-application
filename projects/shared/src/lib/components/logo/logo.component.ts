ec7c385e227873bb1535705039aa423c6ca89080
Aaron Jones II <jonesaaron1996@gmail.com>
2024-07-29 15:07:42 -0400

    feat(shared): logo component

---
+++ projects/shared/src/lib/components/logo/logo.component.ts
@@ -0,0 +1,25 @@
+import {
+  ChangeDetectionStrategy, Component, HostBinding, model,
+} from '@angular/core';
+import { NgOptimizedImage } from "@angular/common";
+
+@Component({
+  selector: 'anon-shared-logo',
+  template: `
+    <img ngSrc="assets/svgs/logo.svg" alt="Logo"
+         [width]="width()" [height]="height()"
+         [priority]="priority()" />
+  `,
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [NgOptimizedImage]
+})
+export class LogoComponent {
+  @HostBinding('attr.aria-hidden') ariaHidden = true;
+
+  width = model(250);
+
+  height = model(250);
+
+  priority = model(false);
+}
