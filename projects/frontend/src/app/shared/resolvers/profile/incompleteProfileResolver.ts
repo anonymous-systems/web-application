9546e90f131d03cfbe081d99a9d157bd36a31f41
Aaron Jones II <jonesaaron1996@gmail.com>
2024-07-28 23:14:40 -0400

    refactor(shared): rename profile resolver to incomplete profile resolver

---
+++ projects/frontend/src/app/shared/resolvers/profile/incompleteProfileResolver.ts
@@ -0,0 +1,16 @@
+import { ResolveFn } from '@angular/router';
+import { inject } from "@angular/core";
+import { AuthService } from "@shared-library";
+import { UserService } from "../../services/user/user.service";
+import { firstValueFrom } from "rxjs";
+
+export const incompleteProfileResolver: ResolveFn<boolean> = async () => {
+  const auth = inject(AuthService);
+  const userAuth = inject(UserService);
+
+  const user = await firstValueFrom(auth.authState$());
+
+  if (!user) return false;
+
+  return !(await userAuth.hasCompletedProfile(user.uid));
+};
