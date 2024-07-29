196ec618d0a4912c513c756dc590a82cb49cecc1
Aaron Jones II <jonesaaron1996@gmail.com>
2024-07-28 23:30:14 -0400

    chore: lint for ci

--- projects/shared/src/lib/services/auth/auth.service.spec.ts
+++ projects/shared/src/lib/services/auth/auth.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { AuthService } from './auth.service';
 import { Auth } from "@angular/fire/auth";
 import { LoggerService } from "../logger/logger.service";
