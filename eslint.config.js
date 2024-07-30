0a1d233c8b86c389c4f2207dbae13698249ecc9e
Aaron Jones II <jonesaaron1996@gmail.com>
2024-07-30 00:58:09 -0400

    feat(frontend): google eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,44 +2,68 @@
 const eslint = require("@eslint/js");
 const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
+const google = require("eslint-config-google");
+
+/**
+ * Modifies the Google ESLint rules configuration for v9 compatibility.
+ *
+ * This function removes specific rules ('valid-jsdoc' and 'require-jsdoc')
+ * that have been removed or deprecated in ESLint v9. By doing so, it ensures
+ * that your project's ESLint configuration remains compatible with the latest
+ * version and avoids potential errors.
+ *
+ * @returns {Record<string, unknown>} A modified version of the Google ESLint
+ *                                   rules with the incompatible rules removed.
+ */
+function getGoogleRulesForV9() {
+    const rules = {...google.rules};
+
+    delete rules['valid-jsdoc'];
+
+    delete rules['require-jsdoc'];
+
+    return rules;
+}
 
 module.exports = tseslint.config(
     {
-      files: ["**/*.ts"],
-      extends: [
-        eslint.configs.recommended,
-        ...tseslint.configs.recommended,
-        ...tseslint.configs.stylistic,
-        ...angular.configs.tsAll,
-      ],
-      processor: angular.processInlineTemplates,
-      rules: {
-        "@angular-eslint/directive-selector": [
-          "error",
-          {
-            type: "attribute",
-            prefix: "anon",
-            style: "camelCase",
-          },
+        files: ["**/*.ts"],
+        extends: [
+            eslint.configs.recommended,
+            ...tseslint.configs.recommended,
+            ...tseslint.configs.stylistic,
+            ...angular.configs.tsAll,
         ],
-        "@angular-eslint/component-selector": [
-          "error",
-          {
-            type: "element",
-            prefix: "anon",
-            style: "kebab-case",
-          },
-        ],
-      },
+        processor: angular.processInlineTemplates,
+        rules: {
+            ...getGoogleRulesForV9(),
+            "@angular-eslint/directive-selector": [
+                "error",
+                {
+                    type: "attribute",
+                    prefix: "anon",
+                    style: "camelCase",
+                },
+            ],
+            "@angular-eslint/component-selector": [
+                "error",
+                {
+                    type: "element",
+                    prefix: "anon",
+                    style: "kebab-case",
+                },
+            ],
+            "new-cap": 0,
+        },
     },
     {
-      files: ["**/*.html"],
-      extends: [
-        ...angular.configs.templateAll,
-      ],
-      rules: {
-        "@angular-eslint/template/no-call-expression": "off",
-        "@angular-eslint/template/i18n": "off",
-      },
+        files: ["**/*.html"],
+        extends: [
+            ...angular.configs.templateAll,
+        ],
+        rules: {
+            "@angular-eslint/template/no-call-expression": "off",
+            "@angular-eslint/template/i18n": "off",
+        },
     }
 );
