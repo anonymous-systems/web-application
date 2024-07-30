a327b458fe4d4a8e4272a2301c157a2c9061eccf
Aaron Jones II <jonesaaron1996@gmail.com>
2024-07-30 18:09:00 -0400

    feat(shared): navigation drawer component

--- projects/shared/src/lib/components/index.ts
+++ projects/shared/src/lib/components/index.ts
@@ -3,3 +3,5 @@ export * from './brand-name/brand-name.component';
 export * from './three-d-sphere/three-d-sphere.component';
 
 export * from './logo/logo.component';
+
+export * from './navigation-drawer/navigation-drawer.component';
---
+++ projects/shared/src/lib/components/navigation-drawer/navigation-drawer.component.ts
@@ -0,0 +1,16 @@
+import { Component, input } from '@angular/core';
+import { GenericItem } from "@shared-library/interfaces";
+import { MatButton } from "@angular/material/button";
+import { RouterLink, RouterLinkActive } from "@angular/router";
+import { MatIcon } from "@angular/material/icon";
+
+@Component({
+  selector: 'anon-navigation-drawer',
+  templateUrl: './navigation-drawer.component.html',
+  styleUrls: ['./navigation-drawer.component.scss'],
+  standalone: true,
+  imports: [MatButton, RouterLinkActive, RouterLink, MatIcon],
+})
+export class NavigationDrawerComponent {
+  segments = input.required<GenericItem[]>();
+}
