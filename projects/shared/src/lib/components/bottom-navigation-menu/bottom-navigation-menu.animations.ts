1900b3f6e9635e55b913ddcbdc019d7b6a3e1356
Aaron Jones II <jonesaaron1996@gmail.com>
2024-07-31 17:13:32 -0400

    fix(shared): use shared animations for navigation drawer and bottom navigation menu animation files

--- projects/shared/src/lib/components/bottom-navigation-menu/bottom-navigation-menu.animations.ts
+++ projects/shared/src/lib/components/bottom-navigation-menu/bottom-navigation-menu.animations.ts
@@ -1,22 +1,19 @@
 import {animate, style, transition, trigger} from '@angular/animations';
+import {
+  emphasizedAccelerate, emphasizedDelecelerate,
+} from '../../../assets/scss/partials/_animations';
 
 export const NavigationMenuAnimation = [
   trigger('navigationMenuAnimation', [
     transition(':enter', [
       style({transform: 'translateY(100%)'}),
       /** Emphasized decelerate */
-      animate(
-          'var(--emphasized-decelerate)',
-          style({transform: 'translateY(0%)'}),
-      ),
+      animate(emphasizedDelecelerate, style({transform: 'translateY(0%)'})),
     ]),
     transition(':leave', [
       style({transform: 'translateY(0%)'}),
       /** Emphasized accelerate */
-      animate(
-          'var(--emphasized-accelerate)',
-          style({transform: 'translateY(100%)'}),
-      ),
+      animate(emphasizedAccelerate, style({transform: 'translateY(100%)'})),
     ]),
   ]),
 ];
