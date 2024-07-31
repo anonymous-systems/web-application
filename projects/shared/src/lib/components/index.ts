15abd321a818d2e740cdf37dc95ac358347bd7b0
Aaron Jones II <jonesaaron1996@gmail.com>
2024-07-31 11:17:35 -0400

    feat(shared): bottom navigation menu component (#17)

--- projects/shared/src/lib/components/index.ts
+++ projects/shared/src/lib/components/index.ts
@@ -5,3 +5,5 @@ export * from './three-d-sphere/three-d-sphere.component';
 export * from './logo/logo.component';
 
 export * from './navigation-drawer/navigation-drawer.component';
+
+export * from './bottom-navigation-menu/bottom-navigation-menu.component';
