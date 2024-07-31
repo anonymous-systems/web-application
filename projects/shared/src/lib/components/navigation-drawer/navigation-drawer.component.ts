9b8bac5e9419372c8eb18dadd2442d04813fae20
Aaron Jones II <jonesaaron1996@gmail.com>
2024-07-31 10:55:31 -0400

    chore(shared): lint for ci

--- projects/shared/src/lib/components/navigation-drawer/navigation-drawer.component.ts
+++ projects/shared/src/lib/components/navigation-drawer/navigation-drawer.component.ts
@@ -1,13 +1,13 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
-import { GenericItem } from "@shared-library/interfaces";
-import { MatButton } from "@angular/material/button";
-import { RouterLink, RouterLinkActive } from "@angular/router";
-import { MatIcon } from "@angular/material/icon";
+import {ChangeDetectionStrategy, Component, input} from '@angular/core';
+import {GenericItem} from '@shared-library/interfaces';
+import {MatButton} from '@angular/material/button';
+import {RouterLink, RouterLinkActive} from '@angular/router';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
-  selector: 'anon-navigation-drawer',
+  selector: 'anon-shared-navigation-drawer',
   templateUrl: './navigation-drawer.component.html',
-  styleUrls: ['./navigation-drawer.component.scss'],
+  styleUrl: './navigation-drawer.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
   imports: [MatButton, RouterLinkActive, RouterLink, MatIcon],
