4e57d04d6e3046b8661d4bf01c1fd0bf32cb4fff
Aaron Jones II <jonesaaron1996@gmail.com>
2024-07-31 13:35:26 -0400

    feat(shared): top app bar component

---
+++ projects/shared/src/lib/components/top-app-bar/top-app-bar.component.ts
@@ -0,0 +1,34 @@
+import {ChangeDetectionStrategy, Component, inject, input} from '@angular/core';
+import {AuthService, BrandNameComponent} from '@shared-library';
+import {RouterLink} from '@angular/router';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {MatMenu, MatMenuTrigger} from '@angular/material/menu';
+import {NgOptimizedImage} from '@angular/common';
+import {MatIcon} from '@angular/material/icon';
+
+@Component({
+  selector: 'anon-shared-top-app-bar',
+  templateUrl: './top-app-bar.component.html',
+  styleUrl: './top-app-bar.component.scss',
+  standalone: true,
+  imports: [
+    RouterLink, BrandNameComponent, MatMenuTrigger, NgOptimizedImage,
+    MatIcon, MatMenu,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TopAppBarComponent {
+  private authService = inject(AuthService);
+
+  readonly title = 'Anonymous Systems';
+
+  homeRoute = input.required<string | string[]>();
+
+  singInRoute = input.required<string | string[]>();
+
+  user = toSignal(this.authService.authState$());
+
+  async signOut() {
+    await this.authService.signOut();
+  }
+}
