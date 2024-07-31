cd53e67524e5f0eb9ff63b58f2398b86b221cd69
Aaron Jones II <jonesaaron1996@gmail.com>
2024-07-31 19:36:34 -0400

    chore:(shared): update auth service mock

--- projects/shared/src/lib/mocks/auth.service.mock.ts
+++ projects/shared/src/lib/mocks/auth.service.mock.ts
@@ -1,9 +1,40 @@
-import {User} from '@angular/fire/auth';
+import {
+  AuthError, IdTokenResult, OperationType, User, UserCredential,
+} from '@angular/fire/auth';
 import {AuthService, GenericItem} from '@shared-library';
+import {Observable, of} from 'rxjs';
 
-export const mockAuthService = jasmine.createSpyObj(
-    'AuthService', ['authState$', 'signOut'],
-) as jasmine.SpyObj<AuthService>;
+export class MockAuthService extends AuthService {
+  override currentUser = () => null;
+
+  override authState$(): Observable<User | null> {
+    return of(null);
+  }
+
+  override signIn() {
+    return Promise.resolve();
+  }
+
+  override googleLogin() {
+    return Promise.resolve(new Error('mock google login error') as AuthError);
+  }
+
+  override signUp() {
+    return Promise.resolve(mockUserCredential);
+  }
+
+  override assertUser() {
+    return;
+  }
+
+  override signOut() {
+    return Promise.resolve();
+  }
+
+  override updateUser() {
+    return Promise.resolve();
+  }
+}
 
 export const mockRoutes: GenericItem[] = [
   {id: '1', name: 'Home'},
@@ -11,7 +42,38 @@ export const mockRoutes: GenericItem[] = [
   {id: '3', name: 'Settings'},
 ];
 
-export const mockUser = {
+export const mockUser: User = {
   uid: 'mock-user-id',
-  displayName: 'Mock User Name',
-} as User;
+  displayName: 'Mock User',
+  emailVerified: true,
+  isAnonymous: false,
+  metadata: {},
+  providerData: [],
+  refreshToken: 'mock-user-refresh-token',
+  tenantId: 'mock-user-tenant-id',
+  email: 'mock-user-email',
+  phoneNumber: 'mock-user-phone-number',
+  photoURL: 'mock-user-photo-url',
+  providerId: 'mock-user-provider-id',
+  delete: () => Promise.resolve(),
+  getIdToken: () => Promise.resolve('mock-user-id-token'),
+  getIdTokenResult: () => Promise.resolve(mockIdTokenResult),
+  reload: () => Promise.resolve(),
+  toJSON: (): object => ({}),
+};
+
+export const mockUserCredential = {
+  user: mockUser,
+  providerId: null,
+  operationType: OperationType.SIGN_IN,
+} satisfies UserCredential;
+
+export const mockIdTokenResult: IdTokenResult = {
+  authTime: '',
+  expirationTime: '',
+  issuedAtTime: '',
+  signInProvider: null,
+  signInSecondFactor: null,
+  token: '',
+  claims: {},
+};
