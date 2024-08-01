ec726c41d91f4ff09f0ef3d9e00050fbbab9a947
Aaron Jones II <jonesaaron1996@gmail.com>
2024-08-01 12:55:36 -0400

    feat(shared): mock firebase file

--- projects/shared/src/lib/mocks/index.ts
+++ projects/shared/src/lib/mocks/index.ts
@@ -1,3 +1,9 @@
 export * from './auth.service.mock';
 
 export * from './breakpoint-observer.mock';
+
+export * from './title.mock';
+
+export * from './meta.mock';
+
+export * from './firebase.mock';
