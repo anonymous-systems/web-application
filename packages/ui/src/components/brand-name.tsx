8f0cd2b70e30694f1e21375dd773218ddbf22fb1
Aaron Jones II <jonesaaron1996@gmail.com>
2025-07-19 03:00:43 -0400

    feat(components): add BrandName component to display company name

---
+++ packages/ui/src/components/brand-name.tsx
@@ -0,0 +1,19 @@
+import { CSSProperties, JSX } from 'react'
+import { CompanyInformation } from '@workspace/ui/lib/company-information.js'
+
+interface Props {
+  className?: string
+  style?: CSSProperties
+}
+export const BrandName = (props: Props): JSX.Element => {
+  const name = CompanyInformation.name
+
+  return (
+    <h2
+      className={`text-2xl ${props.className}`}
+      style={{ color: 'var(--secondary-color)', ...props.style }}
+    >
+      {name}
+    </h2>
+  )
+}
