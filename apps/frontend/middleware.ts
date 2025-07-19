b02243276ee0a7ea1b61821f428aaaf1bf6d18ea
Aaron Jones II <jonesaaron1996@gmail.com>
2025-07-19 03:00:23 -0400

    refactor(middleware): enhance authentication handling with redirects for valid and invalid tokens

--- apps/frontend/middleware.ts
+++ apps/frontend/middleware.ts
@@ -1,6 +1,10 @@
-import { NextRequest } from 'next/server'
-import { authMiddleware } from 'next-firebase-auth-edge'
+import { NextRequest, NextResponse } from 'next/server'
+import { authMiddleware, redirectToHome, redirectToLogin } from 'next-firebase-auth-edge'
 import { authConfig } from '@workspace/firebase-config/auth'
+import { AppRoutes, AuthRoutes, PublicRoutes } from '@/lib/app-routes'
+
+const AUTH_PATHS = Object.values(AuthRoutes)
+const PUBLIC_PATHS = Object.values(PublicRoutes)
 
 export const middleware = async (request: NextRequest) => {
   return authMiddleware(request, {
@@ -12,10 +16,31 @@ export const middleware = async (request: NextRequest) => {
     cookieSerializeOptions: authConfig.cookieSerializeOptions,
     enableMultipleCookies: authConfig.enableMultipleCookies,
     enableCustomToken: authConfig.enableCustomToken,
-    serviceAccount: authConfig.serviceAccount
-  });
+    serviceAccount: authConfig.serviceAccount,
+    handleValidToken: async (_, headers) => {
+      if (AUTH_PATHS.includes(request.nextUrl.pathname)) {
+        console.debug('User is authenticated, redirecting to home page')
+        return redirectToHome(request)
+      }
+
+      return NextResponse.next({ request: { headers } })
+    },
+    handleInvalidToken: async (reason) => {
+      // console.debug('Missing or malformed credentials', {reason})
+      if (!PUBLIC_PATHS.includes(request.nextUrl.pathname)) {
+        console.debug('User is not authenticated to access this page', { reason })
+        return redirectToLogin(request, { path: AppRoutes.signIn, publicPaths: PUBLIC_PATHS })
+      }
+      return NextResponse.next()
+    },
+    handleError: async (error) => {
+      console.error('Unhandled authentication error', error)
+
+      return redirectToLogin(request, { path: AppRoutes.signIn, publicPaths: PUBLIC_PATHS })
+    }
+  })
 }
 
 export const config = {
   matcher: ["/api/login", "/api/logout", "/", "/((?!_next|favicon.ico|api|.*\\.).*)"],
-};
+}
