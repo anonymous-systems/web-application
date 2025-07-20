d046046a13aca21d598a83c6fa600c08ed7a3261
Aaron Jones II <jonesaaron1996@gmail.com>
2025-07-20 00:55:16 -0400

    refactor(homepage): replace TestAuth with dynamic route links for improved navigation

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,6 +1,8 @@
 import { CompanyInformation } from '@workspace/ui/lib/company-information'
 import { ThemeToggle } from '@workspace/ui/components/theme-toggle'
-import { TestAuth } from '@/components/TestAuth'
+import Link from 'next/link'
+import { AppRoutes } from '@/lib/app-routes'
+import { Button } from '@workspace/ui/components/button'
 
 export default function HomePage() {
   const { name } = CompanyInformation
@@ -11,7 +13,12 @@ export default function HomePage() {
         <h1 className="huge">{name}</h1>
         <h2 className='h2 text-muted-foreground'>Frontend Coming soon...</h2>
         <ThemeToggle />
-        <TestAuth />
+        <h3 className='h3'>Test Routes below:</h3>
+        {Object.entries(AppRoutes).map(([k,v]) => (
+          <Link key={k} href={v}>
+          <Button>{k}</Button>
+        </Link>
+        ))}
       </div>
     </div>
   )
