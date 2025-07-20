7b4b9dd45d9c9e07a3408e73745d2c1bfb23bbdb
Aaron Jones II <jonesaaron1996@gmail.com>
2025-07-20 03:01:02 -0400

    refactor(home-page): refactor to use Layout component

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,16 +1,13 @@
-import { CompanyInformation } from '@workspace/ui/lib/company-information'
 import { ThemeToggle } from '@workspace/ui/components/theme-toggle'
 import Link from 'next/link'
 import { AppRoutes } from '@/lib/app-routes'
 import { Button } from '@workspace/ui/components/button'
+import { Layout } from '@/components/layout'
 
 export default function HomePage() {
-  const { name } = CompanyInformation
-
   return (
-    <div className="grid place-content-center min-h-svh">
+    <Layout>
       <div className="flex flex-col items-center gap-8 text-center">
-        <h1 className="huge">{name}</h1>
         <h2 className='h2 text-muted-foreground'>Frontend Coming soon...</h2>
         <ThemeToggle />
         <h3 className='h3'>Test Routes below:</h3>
@@ -20,6 +17,6 @@ export default function HomePage() {
         </Link>
         ))}
       </div>
-    </div>
+    </Layout>
   )
 }
