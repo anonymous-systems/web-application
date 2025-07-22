0feaa87ee3a44ff57db40549bbc59cd5648fc7cb
Aaron Jones II <jonesaaron1996@gmail.com>
2025-07-21 20:25:29 -0400

    refactor(layout): refactor layout to use Nav component with dynamic navigation links

--- apps/frontend/components/layout.tsx
+++ apps/frontend/components/layout.tsx
@@ -1,10 +1,22 @@
 'use client'
 
 import { JSX, ReactNode, useState } from 'react'
-import { TopAppBar } from '@/components/top-app-bar'
 import { useAuth } from '@/hooks/use-auth'
 import { toast } from '@workspace/ui/components/sonner'
 import { useRouter } from 'next/navigation'
+import { Nav } from '@workspace/ui/components/nav'
+import { AppRoutes } from '@/lib/app-routes'
+import { BrandName } from '@workspace/ui/components/brand-name'
+import Link from 'next/link'
+import { Button } from '@workspace/ui/components/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent, DropdownMenuItem,
+  DropdownMenuTrigger
+} from '@workspace/ui/components/dropdown-menu'
+import { Avatar, AvatarFallback, AvatarImage } from '@workspace/ui/components/avatar'
+import { LogOut } from 'lucide-react'
+import { NavLink } from '@workspace/ui/models/interfaces/nav-link'
 
 interface Props {
   children: ReactNode
@@ -26,10 +38,56 @@ export const Layout = (props: Props): JSX.Element => {
     setIsLoading(false)
   }
 
+  const navLinks: NavLink[] = [
+    ...Array(4).fill(Object.entries(AppRoutes)).flat().map(([k,v]) => ({
+      id: k,
+      name: k,
+      href: v,
+    }))
+  ]
+
   return (
-    <div>
-      <TopAppBar user={user} onSignOut={handleSignOut} isLoading={isLoading} />
-      <main>{props.children}</main>
-    </div>
+    <Nav
+      navLinks={navLinks}
+      content={
+        <>
+          <Link href={AppRoutes.home}>
+            <BrandName />
+          </Link>
+
+          <div className='flex-grow flex justify-end'>
+            {user == null
+            ? (
+              <Link href={AppRoutes.signIn}>
+                <Button variant='link'>Sign in</Button>
+              </Link>
+            )
+            : (
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild disabled={isLoading}>
+                  <Avatar className='cursor-pointer'>
+                    <AvatarImage
+                      src={user.photoURL ?? undefined}
+                      alt={user.displayName ?? undefined}
+                    />
+                    <AvatarFallback>
+                      {(user.displayName ?? user.email ?? '')[0]?.toUpperCase()}
+                    </AvatarFallback>
+                  </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuItem onSelect={handleSignOut} disabled={isLoading}>
+                    <LogOut className='mr-4' />
+                    Sign Out
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            )}
+          </div>
+        </>
+      }
+    >
+      <main>{props.children}<div className='min-h-svh'></div></main>
+    </Nav>
   )
 }
