d1edc111546cf2951bba0d6a8a3deaa969283474
Aaron Jones II <jonesaaron1996@gmail.com>
2025-07-23 14:40:12 -0400

    refactor(eslint): consolidate ESLint configuration for React and Next.js

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -2,11 +2,11 @@ import js from "@eslint/js"
 import pluginNext from "@next/eslint-plugin-next"
 import eslintConfigPrettier from "eslint-config-prettier"
 import pluginReact from "eslint-plugin-react"
-import pluginReactHooks from "eslint-plugin-react-hooks"
 import globals from "globals"
 import tseslint from "typescript-eslint"
 
 import { config as baseConfig } from "./base.js"
+import {reactAndNextJsConfig} from "./react-and-next.js";
 
 /**
  * A custom ESLint configuration for libraries that use Next.js.
@@ -40,35 +40,5 @@ export const nextJsConfig = [
       ...pluginNext.configs["core-web-vitals"].rules,
     },
   },
-  {
-    plugins: {
-      "react-hooks": pluginReactHooks,
-    },
-    settings: { react: { version: "detect" } },
-    rules: {
-      ...pluginReactHooks.configs.recommended.rules,
-      // React scope no longer necessary with new JSX transform.
-      "react/react-in-jsx-scope": "off",
-      "react/prop-types": "off",
-      'no-undef': 'error',
-      'linebreak-style': 0,
-      'object-curly-spacing': [ 'error', 'always' ],
-      quotes: [ 'error', 'single' ],
-      indent: [ 'error', 2 ],
-      semi: [ 'error', 'never' ],
-      '@typescript-eslint/no-misused-promises': [
-        'error',
-        {
-          checksVoidReturn: { attributes: false }
-        }
-      ],
-      '@typescript-eslint/no-extraneous-class': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'error',
-      'react/jsx-uses-react': 'error',
-      'react/jsx-uses-vars': 'error',
-      'react/jsx-indent-props': ['error', 2],
-      'react/jsx-closing-bracket-location': ['error', 'line-aligned'],
-      'react/jsx-indent': ['error', 2, { checkAttributes: true, indentLogicalExpressions: true }],
-    },
-  },
+  ...reactAndNextJsConfig
 ]
--- packages/eslint-config/react-internal.js
+++ packages/eslint-config/react-internal.js
@@ -1,11 +1,11 @@
 import js from "@eslint/js"
 import eslintConfigPrettier from "eslint-config-prettier"
 import pluginReact from "eslint-plugin-react"
-import pluginReactHooks from "eslint-plugin-react-hooks"
 import globals from "globals"
 import tseslint from "typescript-eslint"
 
 import { config as baseConfig } from "./base.js"
+import {reactAndNextJsConfig} from "./react-and-next.js";
 
 /**
  * A custom ESLint configuration for libraries that use React.
@@ -26,16 +26,5 @@ export const config = [
       },
     },
   },
-  {
-    plugins: {
-      "react-hooks": pluginReactHooks,
-    },
-    settings: { react: { version: "detect" } },
-    rules: {
-      ...pluginReactHooks.configs.recommended.rules,
-      // React scope no longer necessary with new JSX transform.
-      "react/react-in-jsx-scope": "off",
-      "react/prop-types": "off",
-    },
-  },
+  ...reactAndNextJsConfig
 ]
