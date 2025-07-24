9301c09d06b3db839bfaf346376c76110a1e6270
Aaron Jones II <jonesaaron1996@gmail.com>
2025-07-24 13:35:14 -0400

    refactor(theme-toggle): add optional className prop to ThemeToggle component

--- packages/ui/src/components/theme-toggle.tsx
+++ packages/ui/src/components/theme-toggle.tsx
@@ -16,12 +16,15 @@ enum THEMES {
   System = 'system'
 }
 
-export const ThemeToggle = (): JSX.Element => {
+interface Props {
+  className?: string
+}
+export const ThemeToggle = (props: Props): JSX.Element => {
   const { setTheme, theme } = useTheme()
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
+      <DropdownMenuTrigger className={props.className} asChild>
         <Button variant="outline" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
