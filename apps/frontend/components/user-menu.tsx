5fecb5e94f8a4cfe07ca30c89e893a5067e48507
Aaron Jones II <jonesaaron1996@gmail.com>
2025-07-24 22:09:19 -0400

    refactor(user-menu): refactor user menu to use UserAvatar and enhance dropdown items

--- apps/frontend/components/user-menu.tsx
+++ apps/frontend/components/user-menu.tsx
@@ -6,11 +6,12 @@ import Link from 'next/link'
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuItem,
+  DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator,
   DropdownMenuTrigger
 } from '@workspace/ui/components/dropdown-menu'
-import { Avatar, AvatarFallback, AvatarImage } from '@workspace/ui/components/avatar'
-import { LogOut } from 'lucide-react'
+import { BadgeQuestionMark, LogOut, UserIcon } from 'lucide-react'
+import { UserAvatar } from '@/components/user-avatar'
+import { GithubIcon } from '@workspace/ui/assets/icons/github-icon'
 
 interface Props {
   user: User | null
@@ -29,17 +30,28 @@ export const UserMenu = (props: Props): JSX.Element => {
   return (
     <DropdownMenu modal={false}>
       <DropdownMenuTrigger asChild disabled={props.isLoading}>
-        <Avatar className='cursor-pointer'>
-          <AvatarImage
-            src={props.user.photoURL ?? undefined}
-            alt={props.user.displayName ?? undefined}
-          />
-          <AvatarFallback>
-            {(props.user.displayName ?? props.user.email ?? '')[0]?.toUpperCase()}
-          </AvatarFallback>
-        </Avatar>
+        <UserAvatar className='cursor-pointer' user={props.user} />
       </DropdownMenuTrigger>
       <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuItem asChild disabled={props.isLoading}>
+          <Link href={AppRoutes.profile}>
+            <UserIcon className='mr-4' />
+            Profile
+          </Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuSeparator />
+        <DropdownMenuItem disabled>
+          <GithubIcon className='mr-4' />
+          GitHub
+        </DropdownMenuItem>
+        <DropdownMenuItem disabled>
+          <BadgeQuestionMark className='mr-4' />
+          Support
+        </DropdownMenuItem>
+
+        <DropdownMenuSeparator />
         <DropdownMenuItem onSelect={props.onSignOut} disabled={props.isLoading}>
           <LogOut className='mr-4' />
           Sign Out
