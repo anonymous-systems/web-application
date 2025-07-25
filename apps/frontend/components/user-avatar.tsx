9f0a55aa95b3b0d5934db65f0a31b7b28af71dac
Aaron Jones II <jonesaaron1996@gmail.com>
2025-07-24 22:09:08 -0400

    feat(components): add UserAvatar component for displaying user profile images

---
+++ apps/frontend/components/user-avatar.tsx
@@ -0,0 +1,23 @@
+import { JSX } from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from '@workspace/ui/components/avatar'
+import { User } from '@/interfaces/user'
+
+interface Props {
+  user: User
+  className?: string
+}
+export const UserAvatar = (props: Props): JSX.Element => {
+  const {user, className, ...restOfProps} = props
+
+  return (
+    <Avatar className={className} {...restOfProps}>
+      <AvatarImage
+        src={user.photoURL ?? undefined}
+        alt={user.displayName ?? undefined}
+      />
+      <AvatarFallback>
+        {(user.displayName ?? user.email ?? '')[0]?.toUpperCase()}
+      </AvatarFallback>
+    </Avatar>
+  )
+}
