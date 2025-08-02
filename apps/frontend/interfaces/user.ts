45437b3d4f76443275b3bdb5880b69843f177a66
Aaron Jones II <jonesaaron1996@gmail.com>
2025-08-02 03:18:41 -0400

    chore(interfaces): import AuthUser type from Firebase for better type safety

--- apps/frontend/interfaces/user.ts
+++ apps/frontend/interfaces/user.ts
@@ -1,7 +1,9 @@
-import { UserInfo } from 'firebase/auth'
+import { UserInfo, type User as AuthUser } from 'firebase/auth'
 import { Claims } from 'next-firebase-auth-edge/auth/claims'
 
 export interface User extends UserInfo {
   emailVerified: boolean
   customClaims: Claims
 }
+
+export { AuthUser }
