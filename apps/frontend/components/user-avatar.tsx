326cc26abb61904cf16c1d210b0df942ef176917
Aaron Jones II <jonesaaron1996@gmail.com>
2025-08-02 03:18:23 -0400

    chore(user-avatar): enhance Avatar component with utility class for better styling

--- apps/frontend/components/user-avatar.tsx
+++ apps/frontend/components/user-avatar.tsx
@@ -1,6 +1,7 @@
 import { JSX } from 'react'
 import { Avatar, AvatarFallback, AvatarImage } from '@workspace/ui/components/avatar'
 import { User } from '@/interfaces/user'
+import { cn } from '@workspace/ui/lib/utils'
 
 interface Props {
   user: User
@@ -10,7 +11,7 @@ export const UserAvatar = (props: Props): JSX.Element => {
   const {user, className, ...restOfProps} = props
 
   return (
-    <Avatar className={className} {...restOfProps}>
+    <Avatar className={cn(className, 'select-none')} {...restOfProps}>
       <AvatarImage
         src={user.photoURL ?? undefined}
         alt={user.displayName ?? undefined}
