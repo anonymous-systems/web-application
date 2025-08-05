6f0d4462adcc1e7365b9806059bf25fc34881272
Aaron Jones II <jonesaaron1996@gmail.com>
2025-08-05 18:30:53 -0400

    refactor(user-service): simplify onboarding function and improve error handling

--- apps/frontend/services/user-service.ts
+++ apps/frontend/services/user-service.ts
@@ -6,25 +6,27 @@ import { getFirebaseFunctions } from '@workspace/firebase-config/client'
 import { toast } from '@workspace/ui/components/sonner'
 
 export const onboardUser = async (userProfile: UserProfile): Promise<boolean> => {
-  try {
-    const onboardUserFn = httpsCallable<typeof userProfile, boolean>(
-      getFirebaseFunctions(),
-      'user-onboard',
-      { limitedUseAppCheckTokens: false }
-    )
-    const results = await onboardUserFn(userProfile)
-      .catch(error => { throw error })
-    return results.data
-  } catch (error) {
-    const { code, message } = error as FunctionsError
-    const toastMessage = [
-      'already-exists', 'failed-precondition', 'invalid-argument', 'unauthenticated'
-    ].includes(code)
-      ? message
-      : 'Uh oh! Something went wrong while onboarding.'
+  const onboardUserFn = httpsCallable<typeof userProfile, boolean>(
+    getFirebaseFunctions(),
+    'user-onboard',
+    { limitedUseAppCheckTokens: false }
+  )
+  return onboardUserFn(userProfile)
+    .then(res => res.data)
+    .catch((error: FunctionsError)=> {
+      let errorMessage = 'Uh oh! Something went wrong while onboarding.'
 
-    toast.error(toastMessage)
-    console.error(error)
-    return false
-  }
+      if ([
+        'functions/already-exists',
+        'functions/failed-precondition',
+        'functions/invalid-argument',
+        'functions/unauthenticated'
+      ].includes(error.code)) {
+        errorMessage = error.message
+      } else console.error(error)
+
+      toast.error(errorMessage)
+
+      return false
+    })
 }
