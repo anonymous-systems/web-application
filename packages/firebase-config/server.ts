ae80fd1e5abac65f49ec9696c7577a18c2c65b15
Aaron Jones II <jonesaaron1996@gmail.com>
2025-08-09 18:26:14 -0400

    chore(server): remove unused Firebase admin initialization code

--- packages/firebase-config/server.ts
+++ packages/firebase-config/server.ts
@@ -1,5 +1,3 @@
-// import admin from 'firebase-admin';
-
 export const firebaseServerConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   serviceAccount: process.env.FIREBASE_ADMIN_PRIVATE_KEY
@@ -14,29 +12,3 @@ export const firebaseServerConfig = {
     : undefined,
   useSecureCookies: process.env.USE_SECURE_COOKIES === 'true'
 }
-
-// const getFirebaseApp = () => {
-//   if (!firebaseServerConfig.serviceAccount) return admin.initializeApp()
-//
-//   if (process.env.FIREBASE_AUTH_EMULATOR_HOST) {
-//     return admin.initializeApp({
-//       projectId: firebaseServerConfig.serviceAccount.projectId
-//     })
-//   }
-//
-//   return admin.initializeApp({
-//     credential: admin.credential.cert(firebaseServerConfig.serviceAccount)
-//   })
-// }
-//
-// const getFirebaseAdminApp = () => {
-//   if (admin.apps.length > 0) {
-//     return admin.apps[0] as admin.app.App
-//   }
-//
-//   admin.firestore.setLogFunction(console.log)
-//
-//   return getFirebaseApp()
-// }
-//
-// export { getFirebaseAdminApp }
