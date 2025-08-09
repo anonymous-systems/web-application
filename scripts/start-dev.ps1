6c9e04f0974fd9f355fb1374d1d92b9fc271d0bb
Aaron Jones II <jonesaaron1996@gmail.com>
2025-08-09 18:26:24 -0400

    chore(dev): update version and enhance development environment initialization

--- scripts/start-dev.ps1
+++ scripts/start-dev.ps1
@@ -8,13 +8,17 @@
 
 .NOTES
     Author: Aaron Jones
-    Version: 1.0.1
-    Last Updated: 2025-07-28
+    Version: 1.0.2
+    Last Updated: 2025-08-09
     Compatibility: PowerShell 5.1 and later.
 #>
 Write-Host "🚀 Initializing development environment..." -ForegroundColor Cyan
 Write-Host "------------------------------"
 
+Write-Host "Killing Firebase emulator ports... `n" -ForegroundColor Yellow
+.\killPorts.ps1
+
+Write-Host "Starting development servers... `n" -ForegroundColor Green
 pnpm run dev:seed
 
 Write-Host "------------------------------"
