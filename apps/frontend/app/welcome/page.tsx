cfcc44619e0d78cee9cf7abe6534d0c034b72043
Aaron Jones II <jonesaaron1996@gmail.com>
2026-08-08 11:34:48 -0400

    refactor(frontend): remove WelcomePage component and integrate WelcomeCard directly in welcome page

--- apps/frontend/app/welcome/page.tsx
+++ apps/frontend/app/welcome/page.tsx
@@ -1,8 +1,15 @@
 import { JSX } from 'react'
-import { WelcomePage } from '@/app/_pages/welcome'
+import { WelcomeCard } from '@/app/welcome/_components/welcome-card'
 
 const Page = (): JSX.Element => {
-  return <WelcomePage />
+  return (
+    <main
+      className='grid place-items-center h-screen bg-background p-8 pb-22'
+      data-testid='welcomePage'
+    >
+      <WelcomeCard />
+    </main>
+  )
 }
 
 export default Page
